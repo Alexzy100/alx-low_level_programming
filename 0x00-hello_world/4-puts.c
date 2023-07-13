c3c5562e587c3d6f098d14922790e058647e8f14
Alexzy100 <fineb500@gmail.com>
2023-07-13 14:23:26 -0700

    .

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!bin/bash
+#include <stdio.h>
+
+int main() {
+    char* s = "Programming is like building a multilingual puzzle";
+
+    // Write the string and a trailing newline to stdout using puts()
+    puts(s);
+
+    return 0;
+}
