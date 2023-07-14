2f8a4d085a945431544895fb20db364560098093
Alexzy100 <fineb500@gmail.com>
2023-07-14 00:24:39 -0700

    .

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Roses are red,\n"
+	       "Violets are blue,\n"
+	       "Your program is a piece of art,\n"
+	       "And so are you.\n");
+
+	return (0);
+}
