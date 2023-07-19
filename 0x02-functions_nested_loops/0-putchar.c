6b3f1898176ceafd7d9f782f183e4c61373481c6
Alexzy100 <fineb500@gmail.com>
2023-07-19 15:09:39 -0700

    .

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,11 +6,7 @@
  *Prints "_putchar" followed by a new line
  *
  *Return: 0 (Success)
- *main - Entry prints _putchar
  *
- *Prints "_putchar" followed by a new line
- *
- *Return: 0 (Success)
  */
 int main(void)
 {
