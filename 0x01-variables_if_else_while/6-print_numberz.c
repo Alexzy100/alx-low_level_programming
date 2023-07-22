47d8b783921cdea76ce5586550823ba1a10eb78d
Alexzy100 <fineb500@gmail.com>
2023-07-22 14:57:58 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,15 +9,14 @@
  *
  * Return: Always 0 (Success)
  */
+int main(void)
+{
+	int i;
 
-int main(void) {
-  int i;
+	for (i = 0; i <= 9; ++i)
+		putchar(i + '0');
 
-  for (i = 0; i <= 9; ++i) {
-    putchar(i + '0');
-  }
+	putchar('\n');
 
-  putchar('\n');
-
-  return 0;
+	return (0);
 }
