96563dcb8fbf993f08ca7beddb4dbb8b3ab2ba03
Alexzy100 <fineb500@gmail.com>
2023-07-22 15:47:06 -0700

    .

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,21 +11,23 @@
  */
 int main(void)
 {
-	int i, j;
+    int i, j;
 
-	for (i = 0; i <= 9; ++i)
-	{
-		for (j = i + 1; j <= 9; ++j)
-		{
-			putchar(i + '0','j + 0');
-			
-			if (i < 8 || j < 9)
-			{
-				putchar(',',' ');
-			}
-		}
-	}
+    for (i = 0; i <= 9; ++i)
+    {
+        for (j = i + 1; j <= 9; ++j)
+        {
+            putchar(i + '0');
+            putchar(j + '0');
 
-	putchar('\n');
-	return (0);
+            if (i < 8 || j < 9)
+                putchar(',');
+
+            if (i < 8 || j < 9)
+                putchar(' ');
+        }
+    }
+
+    putchar('\n');
+    return (0);
 }
