508ec169252a1391b3aed6270daa3aa82fbc69d1
Alexzy100 <fineb500@gmail.com>
2023-07-22 13:39:15 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,7 +15,7 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n = rand() % 201 - 100; /* Generate a random number between -100 and 100 */
+	n = rand() % 2 - 1; /* Generate a random number between -100 and 100 */
 
 	printf("%d ", n);
 
