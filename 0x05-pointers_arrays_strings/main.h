c064aa9208ef1cbb07f40ee3568b918d2ec7bfaf
Alexzy100 <fineb500@gmail.com>
2023-07-24 09:50:24 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,6 +3,7 @@
 int _putchar(char c);
 void _puts(char *str);
 #endif /* MAIN_H */
+char *_strcpy(char *dest, char *src);
 void print_array(int *a, int n);
 void print_rev(char *s);
 void rev_string(char *s);
@@ -14,4 +15,3 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
-char *_strcpy(char *dest, char *src);
