e871d8b2e2aae7c3741027681f8398e1ae47e84a
Alexzy100 <fineb500@gmail.com>
2023-07-24 13:37:15 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -4,28 +4,30 @@
 
 #define PASSWORD_LENGTH 15
 
-// Function to generate a random character
+/* Function to generate a random character */
 char getRandomChar() {
-    // Characters to choose from for the password
+    /* Characters to choose from for the password */
     char characters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     int len = sizeof(characters) - 1;
     return characters[rand() % len];
 }
 
 int main() {
-    // Seed the random number generator with the current time
+    int i; // Declare i before the loop to conform to ISO C90
+
+    /* Seed the random number generator with the current time */
     srand(time(NULL));
 
-    char password[PASSWORD_LENGTH + 1]; // +1 for null-terminator
+    char password[PASSWORD_LENGTH + 1]; /* +1 for null-terminator */
 
-    // Generate the password
-    for (int i = 0; i < PASSWORD_LENGTH; i++) {
+    /* Generate the password */
+    for (i = 0; i < PASSWORD_LENGTH; i++) {
         password[i] = getRandomChar();
     }
 
-    password[PASSWORD_LENGTH] = '\0'; // Null-terminate the password string
+    password[PASSWORD_LENGTH] = '\0'; /* Null-terminate the password string */
 
-    // Print the generated password
+    /* Print the generated password */
     printf("%s\n", password);
 
     return 0;
