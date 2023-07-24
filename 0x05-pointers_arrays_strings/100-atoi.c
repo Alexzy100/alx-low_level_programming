6d4d4b392d05ad6aea78067018f6f2f142b22ee6
Alexzy100 <fineb500@gmail.com>
2023-07-24 10:19:35 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,21 +1,31 @@
-#include <stdio.h>
-#include <stdlib.h>
-
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: The string to convert.
+ *
+ * Return: The converted integer.
+ */
 int _atoi(char *s) {
-  int sign = 1;
-  int n = 0;
+  /* Skip any leading whitespace characters. */
   while (*s == ' ') {
     s++;
   }
+
+  /* If the first character is a '-' or '+' sign, store the sign and skip the sign character. */
+  int sign = 1;
   if (*s == '-') {
     sign = -1;
     s++;
   } else if (*s == '+') {
     s++;
   }
+
+  /* Convert each digit to an integer and add it to the result. */
+  int n = 0;
   while (*s >= '0' && *s <= '9') {
     n = n * 10 + (*s - '0');
     s++;
   }
+
+  /* Return the result, multiplied by the sign. */
   return n * sign;
 }
