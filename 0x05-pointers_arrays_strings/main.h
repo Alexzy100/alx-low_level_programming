e2e0a7932b5c42ddf2ecd3005246dbd6fe8e6a41
Alexzy100 <fineb500@gmail.com>
2023-07-24 06:09:28 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void _puts(char *str);
+
+#endif /* MAIN_H */
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
