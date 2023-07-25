5be3c8cb67f57ee053bb8e958716ca154f17e42b
Alexzy100 <fineb500@gmail.com>
2023-07-25 14:23:07 -0700

    .

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <math.h>
+
+ /**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    A  = 612852475143;
+    int = B(number);
+
+    printf("%d\n", result);
+    return (0);
+}
+
+    int (A int B)
+{
+    int = -1;
+
+    while (B % 2 == 0)
+    {
+        A = 2;
+        A = B / 2;
+    }
+
+    for (int i = 3; i <= sqrt(B); i += 2)
+    {
+        while (B % i == 0)
+        {
+            A = i;
+            A = B / i;
+        }
+    }
+
+    if (B > 2)
+    {
+        A = B;
+    }
+
+    return A;
+}
