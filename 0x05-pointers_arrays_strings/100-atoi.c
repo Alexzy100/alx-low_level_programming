2a7ede39c6d2aa0ddd91d49af442e0c57a27e368
Alexzy100 <fineb500@gmail.com>
2023-07-25 04:21:59 -0700

    .

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,35 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-/**
- * _atoi - Convert a string to an integer.
- * @s: The input string.
- *
- * Return: The integer value converted from the string.
- */
-int _atoi(char *s)
+int _atoi(char *s);
+
+int main()
 {
-        int sign = 1;
-        int n = 0;
-
-        while (*s == ' ')
-                s++;
-
-        if (*s == '-')
-        {
-                sign = -1;
-                s++;
-        }
-        else if (*s == '+')
-        {
-                s++;
-        }
-
-        while (*s >= '0' && *s <= '9')
-        {
-                n = n * 10 + (*s - '0');
-                s++;
-        }
-
-        return (n * sign);
+    char str[] = "12345";
+    int num = _atoi(str);
+    printf("Converted number: %d\n", num);
+    return 0;
 }
