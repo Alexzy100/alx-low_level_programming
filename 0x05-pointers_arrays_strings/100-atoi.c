b1054b42192861b81517acf3a696ce5750e9adfb
Alexzy100 <fineb500@gmail.com>
2023-07-25 05:25:16 -0700

    .

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,49 +1,34 @@
-I#include <stdbool.h>
-
 /**
  * _atoi - Converts a string to an integer.
  *
- * @param nptr The string to convert.
+ * @param s The string to convert.
  *
  * @return The integer representation of the string.
  *
  * If the string does not contain a valid integer, the function returns 0.
+ *
+ * @author Bard
  */
-int _atoi(const char *nptr) {
-    int sign = 1;
-    int n = 0;
-    bool found_digit = false;
+int _atoi(char *s) {
+  int sign = 1;
+  int n = 0;
 
-    /* Skip over any non-digit characters and handle signs */
-    while (*nptr) {
-        if (*nptr == '-') {
-            sign = -sign;
-            nptr++;
-        } else if (*nptr == '+') {
-            nptr++;
-        } else if (*nptr >= '0' && *nptr <= '9') {
-            found_digit = true;
-            break;
-        } else {
-            /* Skip non-digit characters */
-            nptr++;
-        }
-    }
+  while (*s == ' ') {
+    s++;
+  }
 
-    /* Parse the integer value */
-    while (*nptr >= '0' && *nptr <= '9') {
-        /* Handling potential signed integer overflow using __builtin_mul_overflow */
-        if (__builtin_mul_overflow(n, 10, &n) || __builtin_add_overflow(n, (*nptr - '0'), &n)) {
-            /* Overflow occurred, return 0 as per the function description */
-            return 0;
-        }
-        nptr++;
-    }
+  if (*s == '-') {
+    sign = -1;
+    s++;
+  } else if (*s == '+') {
+    sign = 1;
+    s++;
+  }
 
-    /* If no digits were found, return 0 */
-    if (!found_digit) {
-        return 0;
-    }
+  while (*s >= '0' && *s <= '9') {
+    n = n * 10 + (*s - '0');
+    s++;
+  }
 
-    return sign * n;
+  return sign * n;
 }
