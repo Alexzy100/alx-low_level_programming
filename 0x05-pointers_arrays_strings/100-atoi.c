27e4d3fc9d134b9fcaac8d294fff9e2a29ba94a9
Alexzy100 <fineb500@gmail.com>
2023-07-25 04:13:21 -0700

    .

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,34 +1,50 @@
-#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
 
-/**
- * _atoi - Convert a string to an integer.
- * @s: The input string.
- *
- * Return: The integer value converted from the string.
- */
+int _atoi(char *s);
+
+int main()
+{
+    char str[] = "1234567";
+    int num = _atoi(str);
+    printf("Converted number: %d\n", num);
+    return 0;
+}
+
 int _atoi(char *s)
 {
-        int sign = 1;
-        int n = 0;
+    int sign = 1;
+    int n = 0;
+    int digit;
 
-        while (*s == ' ')
-                s++;
+    while (*s == ' ')
+        s++;
 
-        if (*s == '-')
+    if (*s == '-')
+    {
+        sign = -1;
+        s++;
+    }
+    else if (*s == '+')
+    {
+        s++;
+    }
+
+    while (*s >= '0' && *s <= '9')
+    {
+        digit = *s - '0';
+
+        // Check for potential overflow before updating n
+        if (n > (INT_MAX - digit) / 10)
         {
-                sign = -1;
-                s++;
-        }
-        else if (*s == '+')
-        {
-                s++;
+            // Handle overflow here (you can return an error value or take appropriate action)
+            // For now, let's return 0 to indicate an error
+            return 0;
         }
 
-        while (*s >= '0' && *s <= '9')
-        {
-                n = n * 10 + (*s - '0');
-                s++;
-        }
+        n = n * 10 + digit;
+        s++;
+    }
 
-        return (n * sign);
+    return (n * sign);
 }
