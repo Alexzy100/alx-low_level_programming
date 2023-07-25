bc4fd295616ffa9cd9f5e528cda137ba377c3a00
Alexzy100 <fineb500@gmail.com>
2023-07-25 04:32:06 -0700

    .

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,3 +1,12 @@
+/**
+ * _atoi - Converts a string to an integer.
+ *
+ * @param s The string to convert.
+ *
+ * @return The integer representation of the string.
+ *
+ * If the string does not contain a valid integer, the function returns 0.
+ */
 int _atoi(char *s) {
   int sign = 1;
   int n = 0;
