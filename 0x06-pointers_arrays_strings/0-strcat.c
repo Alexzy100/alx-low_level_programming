66c845f271cfc96f6f6a98f229576bfb8552b231
Alexzy100 <fineb500@gmail.com>
2023-07-26 14:04:30 -0700

    .

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,26 +1,24 @@
-/*
- * _strcat - concatenates two strings
+/**
+ * _strcat - Concatenates two strings.
+ * @dest: The destination string.
+ * @src: The source string.
  *
- * @dest: the destination string
- * @src: the source string
- *
- * Returns: a pointer to the destination string
+ * Return: A pointer to the destination string.
  */
-char *_strcat(char *dest, char *src) {
-  int i, j;
+char *_strcat(char *dest, char *src)
+{
+    int i, j;
 
-  /* Find the end of the destination string */
-  for (i = 0; dest[i] != '\0'; i++) {
-  }
+    for (i = 0; dest[i] != '\0'; i++)
+    {
+    }
 
-  /* Iterate through the source string, copying each character to the destination string */
-  for (j = 0; src[j] != '\0'; j++) {
-    dest[i + j] = src[j];
-  }
+    for (j = 0; src[j] != '\0'; j++)
+    {
+        dest[i + j] = src[j];
+    }
 
-  /* Append a null character to the destination string */
-  dest[i + j] = '\0';
+    dest[i + j] = '\0';
 
-  /* Return a pointer to the destination string */
-  return dest;
+    return dest;
 }
