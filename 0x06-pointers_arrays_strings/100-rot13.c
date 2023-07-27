9e09fd4b916175d8ca485a6e3ab739b62d23218d
Alexzy100 <fineb500@gmail.com>
2023-07-27 06:50:47 -0700

    .

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,23 +1,33 @@
 #include <stdio.h>
 
-char *rot13(char *s) {
-    char *ptr = s;
+/**
+ * rot13 - Encodes a string using ROT13 substitution cipher.
+ * @s: The input string to be encoded.
+ *
+ * Return: A pointer to the encoded string.
+ */
+char *rot13(char *s)
+{
+	char *ptr = s;
 
-    while (*ptr != '\0') {
-        char ch = *ptr;
+	while (*ptr != '\0')
+	{
+		char ch = *ptr;
 
-        /* Check if the character is an uppercase letter */
-        if (ch >= 'A' && ch <= 'Z') {
-            ch = ((ch - 'A' + 13) % 26) + 'A';
-        }
-        /* Check if the character is a lowercase letter */
-        else if (ch >= 'a' && ch <= 'z') {
-            ch = ((ch - 'a' + 13) % 26) + 'a';
-        }
+		/* Check if the character is an uppercase letter */
+		if (ch >= 'A' && ch <= 'Z')
+		{
+			ch = ((ch - 'A' + 13) % 26) + 'A';
+		}
+		/* Check if the character is a lowercase letter */
+		else if (ch >= 'a' && ch <= 'z')
+		{
+			ch = ((ch - 'a' + 13) % 26) + 'a';
+		}
 
-        *ptr = ch;
-        ptr++;
-    }
+		*ptr = ch;
+		ptr++;
+	}
 
-    return s;
+	return (s);
 }
