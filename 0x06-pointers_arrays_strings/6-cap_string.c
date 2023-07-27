d13330653c498c0acfd26b3bf9eb551953a8fb08
Alexzy100 <fineb500@gmail.com>
2023-07-27 06:30:36 -0700

    .

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,22 +1,30 @@
-#include <stdio.h>
-#include <string.h>
+#include <stddef.h>
 
-char *cap_string(char *str)
+/**
+ * cap_string - Capitalizes all words of a string
+ * @s: The input string
+ *
+ * Return: Pointer to the modified string
+ */
+char *cap_string(char *s)
 {
-  int i;
-  for (i = 0; str[i] != '\0'; i++)
-  {
-    if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' ||
-        str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';' ||
-        str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' ||
-        str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' ||
-        str[i - 1] == '{' || str[i - 1] == '}')
-    {
-      if (str[i] >= 'a' && str[i] <= 'z')
-      {
-        str[i] -= 32;
-      }
-    }
-  }
-  return str;
+	char *result = s;
+	int i = 0;
+
+	while (s[i])
+	{
+		if (i == 0 || s[i - 1] == ' ' || s[i - 1] == '\t' ||
+		    s[i - 1] == '\n' || s[i - 1] == ',' || s[i - 1] == ';' ||
+		    s[i - 1] == '.' || s[i - 1] == '!' || s[i - 1] == '?' ||
+		    s[i - 1] == '"' || s[i - 1] == '(' || s[i - 1] == ')' ||
+		    s[i - 1] == '{' || s[i - 1] == '}')
+		{
+			if (s[i] >= 'a' && s[i] <= 'z')
+				s[i] -= 32;
+		}
+
+		i++;
+	}
+
+	return (result);
 }
