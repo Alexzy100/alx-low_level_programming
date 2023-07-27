8612d83b84a828f1920311fbfeb826fbb916f977
Alexzy100 <fineb500@gmail.com>
2023-07-27 04:05:19 -0700

    .

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,23 +1,34 @@
 #include <ctype.h>
 
 /**
- * cap_string - Capitalizes all words of a string.
+ * cap_string - Capitalizes the first letter of each word in a string.
  * @str: The string to capitalize.
  *
  * Return: A pointer to the modified string.
  */
-char *cap_string(char *str) {
-  int i;
-  int is_word = 1;
+char *cap_string(char *str)
+{
+	int i;
+	int is_word = 1;
 
-  for (i = 0; str[i]; i++) {
-    if (isspace(str[i])) {
-      is_word = 1;
-    } else if (is_word) {
-      str[i] = toupper(str[i]);
-      is_word = 0;
-    }
-  }
+	/* Capitalize the first character, if it's a lowercase letter */
+	if (str[0] && islower(str[0]))
+	{
+		str[0] = toupper(str[0]);
+	}
 
-  return str;
+	for (i = 1; str[i]; i++)
+	{
+		if (isspace(str[i]) || !isalpha(str[i - 1]))
+		{
+			is_word = 1;
+		}
+		else if (is_word)
+		{
+			str[i] = toupper(str[i]);
+			is_word = 0;
+		}
+	}
+
+	return (str);
 }
