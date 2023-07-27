551179b1a6e9f060af8dbc421e1b45cacadb095c
Alexzy100 <fineb500@gmail.com>
2023-07-27 03:49:03 -0700

    .

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,19 +4,22 @@
  * @s2: Pointer to the second string.
  *
  * Return: Integer value less than, equal to, or greater than zero if
- *         the first string is found, respectively, to be less than,
- *         to match, or be greater than the second string.
+ * the first string is found, respectively, to be less than,
+ * to match, or be greater than the second string.
  */
 int _strcmp(char *s1, char *s2)
 {
-    int i = 0;
+	/* Remove space at the start of the line */
+	int i = 0;
 
-    while (s1[i] != '\0' && s2[i] != '\0') {
-        if (s1[i] != s2[i])
-            return s1[i] - s2[i];
-        i++;
-    }
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		/* Fix indentation for conditional statements */
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
 
-    return s1[i] - s2[i];
+		i++;
+	}
+
+	return (s1[i] - s2[i]);
 }
-
