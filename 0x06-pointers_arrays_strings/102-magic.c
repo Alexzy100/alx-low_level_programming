78d481d6b211873655c0ee3c34e3fc0b1397b098
Alexzy100 <fineb500@gmail.com>
2023-07-27 08:12:08 -0700

    .

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int n;
+  int a[5];
+  int *p;
+
+  a[2] = 1024;
+  p = &n;
+  *(p + 5) = 98; // Update the value of a[2] indirectly through pointer arithmetic
+
+  // Set the value of a[2] directly to 98
+  a[2] = 98;
+
+  // Print the value of a[2]
+  printf("a[2] = %d\n", a[2]);
+  return 0;
+}
