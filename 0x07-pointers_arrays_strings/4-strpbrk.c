a582c2efe7b6c226388e48d708e9b678a007cb48
Alexzy100 <fineb500@gmail.com>
2023-07-31 07:54:07 -0700

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+/**
+*_strpbrk - Locates the first occurrence of any of the byte s.
+*@s: Pointer to the string to search.
+*@accept: Pointer to the set of bytes to match.
+*Return: Pointer to the byte in s that matches one of the bytes in accept,
+*a pointer to the null terminator if no such byte is found.
+*/
+char *_strpbrk(char *s, char *accept)
+{
+char *match;
+
+while (*s)
+{
+for (match = accept; *match; match++)
+{
+if (*s == *match)
+{
+return (s);
+}
+}
+s++;
+}
+
+return (s);
+}
